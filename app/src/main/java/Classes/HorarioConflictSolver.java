7ba7b7ca67f49206549a35236e217ca743e53dd2
TheF16 <116013488+TheF16@users.noreply.github.com>
2023-01-03 17:08:19 +0000

    juntar cenas

--- app/src/main/java/Classes/HorarioConflictSolver.java
+++ app/src/main/java/Classes/HorarioConflictSolver.java
@@ -4,25 +4,35 @@ import java.util.ArrayList;
 
 public class HorarioConflictSolver implements Runnable{
     private ArrayList<Comboio> comboios;
+    private Horario horario;
 
     public HorarioConflictSolver(ArrayList<Comboio> comboios) {
         this.comboios = comboios;
     }
 
     private boolean hasConflicts() {
-        
-
+        for (int i = 0; i < this.comboios.size(); i++) {
+            for (int j = i; i < this.comboios.size(); j++) {
+                if ((this.comboios.get(i).getDirecaoComboio() == this.comboios.get(j).getDirecaoComboio()) && (this.comboios.get(i).getHorarioComboio().getHoraPartida() == this.comboios.get(j).getHorarioComboio().getHoraPartida())) {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
 
-    private void solveConflicts() {
-
-    }
-
     public void run() {
         while (this.hasConflicts()) {
-            this.solveConflicts();
+            for (int i = 0; i < this.comboios.size(); i++) {
+                for (int j = i; i < this.comboios.size(); j++) {
+                    if ((this.comboios.get(i).getDirecaoComboio() == this.comboios.get(j).getDirecaoComboio()) && (this.comboios.get(i).getHorarioComboio().getHoraPartida() == this.comboios.get(j).getHorarioComboio().getHoraPartida())) {
+                        this.comboios.get(j).getHorarioComboio().getHoraPartida().plusMinutes(15);
+                        this.comboios.get(j).getHorarioComboio().getHoraChegada().plusMinutes(15);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
